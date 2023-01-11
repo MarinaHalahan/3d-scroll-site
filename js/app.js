a6a76425f0d90aa53ef7ae957748e3de6f456396
MarinaHalahan <ichmarina1@gmail.com>
2023-01-11 23:14:55 +0200

    м

--- js/app.js
+++ js/app.js
@@ -3,11 +3,19 @@
 let zSpacing = -1000;
 let lastPos = zSpacing / 5;
 let mainFrame = document.getElementsByClassName("frame");
-let frame = Array.from(mainFrame);
+let frames = Array.from(mainFrame);
 let zVals = [];
 
 window.onscroll = function () {
   let top = document.documentElement.scrollTop;
   let delta = lastPos - top;
   lastPos = top;
+
+  frames.forEach(function (n, i) {
+    zVals.push(i * zSpacing + zSpacing);
+    zVals[i] += delta * -5;
+    let frame = frames[i];
+    let transform = `translateZ(${zVals[i]}px)`;
+    frame.setAttribute("style", `transform:${transform}`);
+  });
 };
